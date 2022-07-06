f4d466beffe1ec7fc8c139d2ea8a367f48dc171b
Susie <susie8to@gmail.com>
2022-07-07 03:44:02 +0800

    WebMvcConfig

--- src/main/java/org/example/reggie/config/WebMvcConfig.java
+++ src/main/java/org/example/reggie/config/WebMvcConfig.java
@@ -1,11 +1,17 @@
 package org.example.reggie.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+@Slf4j
+@Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        super.addResourceHandlers(registry);
+        log.info("mapping static resources");
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
     }
 }
